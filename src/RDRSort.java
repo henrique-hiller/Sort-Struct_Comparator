6985e922078cd593cf4a8671d9e3ba776969f332
Henrique Hiller <henriquehiller@gmail.com>
2025-04-14 17:40:09 -0300

    Final Commit

--- src/RDRSort.java
+++ src/RDRSort.java
@@ -166,8 +166,7 @@ public class RDRSort {
     }
 
     private long mmcCode(){
-        int actual = 0;
-        long ctr = mmcCodeSort(actual, this.numbers[actual]);
+        long ctr = mmcCodeSort(0, this.numbers[0]);
         long rep = 0;
         long ctr2;
         for (int i =0;i<this.size;i++){
